e6792409f67d0a763935d2c54569c9abad82175d
IAmKou <luongcu111@gmail.com>
2024-09-18 23:21:18 +0700

    tao project

---
+++ src/test/java/com/example/jip/JipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
