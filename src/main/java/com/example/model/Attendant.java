cb4ea8f30aaef429786bed0be699705183dc6772
IAmKou <luongcu111@gmail.com>
2024-10-08 00:53:58 +0700

    Create Attendant.java

---
+++ src/main/java/com/example/model/Attendant.java
@@ -0,0 +1,71 @@
+package com.example.model;
+
+import java.sql.Date;
+
+public class Attendant {
+    private int id;
+    private int student_id;
+    private int schedule_id;
+    private String status;
+    private Date date;
+    private String note;
+
+    public Attendant(){}
+
+    public Attendant(int id, int student_id, int schedule_id, String status, Date date, String note) {
+        this.id = id;
+        this.student_id = student_id;
+        this.schedule_id = schedule_id;
+        this.status = status;
+        this.date = date;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public int getSchedule_id() {
+        return schedule_id;
+    }
+
+    public void setSchedule_id(int schedule_id) {
+        this.schedule_id = schedule_id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
