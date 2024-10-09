7766c47ee2cd53fbfdfd00a3f586dd1a13f06013
IAmKou <luongcu111@gmail.com>
2024-10-10 02:04:27 +0700

    Create TeacherRepository.java

---
+++ src/main/java/com/example/jip/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package com.example.jip.repository;
+
+import com.example.jip.entity.Teacher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
+}
