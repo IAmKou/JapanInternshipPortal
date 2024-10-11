8f5c26807843b174dc69c67199b8840711c4017e
IAmKou <luongcu111@gmail.com>
2024-10-11 21:15:11 +0700

    Create RoleRepository.java

---
+++ src/main/java/com/example/jip/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.jip.repository;
+
+import com.example.jip.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
