1c45aff8fe8fb3113d1034e1bd09eb71f20ba153
IAmKou <luongcu111@gmail.com>
2024-10-16 01:58:20 +0700

    Update StudentServices.java

--- src/main/java/com/example/jip/services/StudentServices.java
+++ src/main/java/com/example/jip/services/StudentServices.java
@@ -1,16 +1,16 @@
 package com.example.jip.services;
 
 import com.example.jip.entity.Student;
-import com.example.jip.repository.StudentReposiroty;
+import com.example.jip.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServices {
     @Autowired
-    private StudentReposiroty studentReposiroty;
+    private StudentRepository studentRepository;
 
     public Student createStudent(Student student) {
-        return studentReposiroty.save(student);
+        return studentRepository.save(student);
     }
 }
