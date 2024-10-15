1f2e422a78fefa0d6cec4e5ed32208e17c8fdcdc
IAmKou <luongcu111@gmail.com>
2024-10-16 01:58:22 +0700

    Update Teacher.java

--- src/main/java/com/example/jip/entity/Teacher.java
+++ src/main/java/com/example/jip/entity/Teacher.java
@@ -12,6 +12,7 @@ public class Teacher {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String Fullname;
+    private String Jname;
     private String email;
     private String PhoneNumber;
     private gender Gender;
@@ -20,9 +21,10 @@ public class Teacher {
 
     public Teacher(){}
 
-    public Teacher(int id, String fullname, String email, String phoneNumber, gender gender, String img, int account_id) {
+    public Teacher(int id, String fullname, String jname, String email, String phoneNumber, gender gender, String img, int account_id) {
         this.id = id;
         Fullname = fullname;
+        Jname = jname;
         this.email = email;
         PhoneNumber = phoneNumber;
         Gender = gender;
