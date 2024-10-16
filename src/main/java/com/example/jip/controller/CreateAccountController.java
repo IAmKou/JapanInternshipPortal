a0120ded430c61484623817244b70ecc9500105f
IAmKou <luongcu111@gmail.com>
2024-10-16 20:44:03 +0700

    Update CreateAccountController.java

--- src/main/java/com/example/jip/controller/CreateAccountController.java
+++ src/main/java/com/example/jip/controller/CreateAccountController.java
@@ -4,6 +4,7 @@ import com.example.jip.entity.Account;
 import com.example.jip.repository.AccountRepository;
 import com.example.jip.services.AccountServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +15,11 @@ public class CreateAccountController {
     private AccountServices accountServices;
 
     @PostMapping("/create")
-    public Account createAccount(@RequestBody Account account, @RequestParam String role) {
-        return accountServices.saveAccount(account,role);
+    public ResponseEntity<Integer> createAccount(@RequestParam String username
+    , @RequestParam String password
+    , @RequestParam int role) {
+        int accountId = accountServices.createAccount(username,password,role);
+        return ResponseEntity.ok(accountId);
+
     }
 }
