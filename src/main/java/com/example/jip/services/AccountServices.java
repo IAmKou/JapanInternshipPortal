ec06b303921bfb1d169fb1c75b23e43c0e2ce039
IAmKou <luongcu111@gmail.com>
2024-10-31 21:17:47 +0700

    Update AccountServices.java

--- src/main/java/com/example/jip/services/AccountServices.java
+++ src/main/java/com/example/jip/services/AccountServices.java
@@ -1,5 +1,7 @@
+// AccountServices.java
 package com.example.jip.services;
 
+import com.example.jip.dto.AccountDTO;
 import com.example.jip.entity.Account;
 import com.example.jip.entity.Role;
 import com.example.jip.repository.AccountRepository;
@@ -8,7 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountServices {
@@ -28,13 +31,17 @@ public class AccountServices {
         account.setPassword(passwordEncoder.encode(password));
 
         Role roleEntity = roleRepository.findById(role)
-                .orElseThrow(()-> new RuntimeException("Role not found"));
+                .orElseThrow(() -> new RuntimeException("Role not found"));
         account.setRole(roleEntity);
 
         Account savedAccount = accountRepository.save(account);
         return savedAccount.getId();
     }
 
-
-
+    public List<AccountDTO> getAllAccountDTOs() {
+        List<Account> accounts = (List<Account>) accountRepository.findAll();
+        return accounts.stream()
+                .map(AccountDTO::new)
+                .collect(Collectors.toList());
+    }
 }
