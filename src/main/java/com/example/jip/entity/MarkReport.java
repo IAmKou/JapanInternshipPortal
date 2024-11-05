48816410cccdc97959ceac536a15cbb587cebc55
IAmKou <luongcu111@gmail.com>
2024-11-05 22:25:52 +0700

    update markReport

--- src/main/java/com/example/jip/entity/MarkReport.java
+++ src/main/java/com/example/jip/entity/MarkReport.java
@@ -1,15 +1,20 @@
 package com.example.jip.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-
+@Entity
 @Setter
 @Getter
 public class MarkReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private int student_id; // Giữ nguyên tên thuộc tính là student_id
+
+    @Column(name="student_id")
+    private int studentId; // Giữ nguyên tên thuộc tính là student_id
     private BigDecimal attendant_rate;
     private BigDecimal avg_assignment_mark;
     private BigDecimal avg_exam_mark;
@@ -19,9 +24,9 @@ public class MarkReport {
 
     public MarkReport() {}
 
-    public MarkReport(int id, int student_id, BigDecimal attendant_rate, BigDecimal avg_assignment_mark, BigDecimal avg_exam_mark, BigDecimal reading_mark, BigDecimal listening_mark, BigDecimal speaking_mark) {
+    public MarkReport(int id, int studentId, BigDecimal attendant_rate, BigDecimal avg_assignment_mark, BigDecimal avg_exam_mark, BigDecimal reading_mark, BigDecimal listening_mark, BigDecimal speaking_mark) {
         this.id = id;
-        this.student_id = student_id;
+        this.studentId = studentId;
         this.attendant_rate = attendant_rate;
         this.avg_assignment_mark = avg_assignment_mark;
         this.avg_exam_mark = avg_exam_mark;
