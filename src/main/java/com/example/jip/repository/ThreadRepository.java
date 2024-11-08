eca706766c1834b63e92a12413f7e02598985182
toobi-svg <richchoi6@gmail.com>
2024-11-08 09:11:35 +0700

    fix error

--- src/main/java/com/example/jip/repository/ThreadRepository.java
+++ src/main/java/com/example/jip/repository/ThreadRepository.java
@@ -2,8 +2,10 @@ package com.example.jip.repository;
 
 import com.example.jip.entity.Thread;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 
 public interface ThreadRepository extends CrudRepository<Thread, Integer> {
     Optional<Thread> findById(Integer id);
