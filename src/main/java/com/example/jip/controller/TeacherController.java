38e4c7119a92c90592b9d1e4f20a22af6ea948ac
IAmKou <luongcu111@gmail.com>
2024-11-09 17:17:57 +0700

    Create TeacherController.java

---
+++ src/main/java/com/example/jip/controller/TeacherController.java
@@ -0,0 +1,24 @@
+package com.example.jip.controller;
+
+
+import com.example.jip.entity.Teacher;
+import com.example.jip.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teachers")
+public class TeacherController {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @GetMapping
+    public List<Teacher> getAllTeacher(){
+        return teacherRepository.findAll();
+    }
+}
