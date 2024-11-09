8f17788776507cc6a5d9df32cd544045714d4c69
IAmKou <luongcu111@gmail.com>
2024-11-09 17:18:08 +0700

    Update StudentRepository.java

--- src/main/java/com/example/jip/repository/StudentRepository.java
+++ src/main/java/com/example/jip/repository/StudentRepository.java
@@ -1,13 +1,20 @@
 package com.example.jip.repository;
 
 import com.example.jip.entity.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
     Optional<Student> findById(Integer id);
     Optional<Student> findByEmail(String email);
     Optional<Student> findByPhoneNumber(String phoneNumber);
+
+    @Query("SELECT s FROM Student s WHERE s.id NOT IN (SELECT l.id.student_id FROM List l) ORDER BY s.fullname ASC limit 30")
+    List<Student> findTop30UnassignedStudents();
+
 
 }
