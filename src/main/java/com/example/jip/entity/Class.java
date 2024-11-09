5d5e1f78ed19428f39b822822d2f7e63707ff33c
IAmKou <luongcu111@gmail.com>
2024-11-09 17:17:59 +0700

    Update Class.java

--- src/main/java/com/example/jip/entity/Class.java
+++ src/main/java/com/example/jip/entity/Class.java
@@ -1,23 +1,34 @@
 package com.example.jip.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
+@Table(name = "Class")
 @Setter
 @Getter
 public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "Name", nullable = false)
     private String name;
+
+    @Column(name = "number_of_student", nullable = false)
     private int number_of_student;
-    private int teacher_id;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private Teacher teacher;
 
     public Class(){}
 
-    public Class(int id, String name, int number_of_student, int teacher_id) {
+    public Class(int id, String name, int number_of_student, Teacher teacher) {
         this.id = id;
         this.name = name;
         this.number_of_student = number_of_student;
-        this.teacher_id = teacher_id;
+        this.teacher = teacher;
     }
 
 }
