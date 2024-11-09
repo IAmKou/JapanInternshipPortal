e999572bbad5c3506ee25642cfd439502242a100
IAmKou <luongcu111@gmail.com>
2024-11-09 17:18:01 +0700

    Update List.java

--- src/main/java/com/example/jip/entity/List.java
+++ src/main/java/com/example/jip/entity/List.java
@@ -1,19 +1,32 @@
 package com.example.jip.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
+@Table(name = "List")
 @Setter
 @Getter
 public class List {
-    private int class_id;
-    private int student_id;
+
+    @EmbeddedId
+    private ListId id;
+
+    @ManyToOne
+    @MapsId("class_id")
+    @JoinColumn(name = "class_id")
+    private Class clas;
+
+    @ManyToOne
+    @MapsId("student_id")
+    @JoinColumn(name = "student_id")
+    private Student student;
 
     public List(){}
 
-    public List(int class_id, int student_id) {
-        this.class_id = class_id;
-        this.student_id = student_id;
+    public List(ListId id, Class clas, Student student) {
+        this.id = id;
+        this.clas = clas;
+        this.student = student;
     }
-
 }
