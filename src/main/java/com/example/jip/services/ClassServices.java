a24235153d330d6d2d3530726c3f809e4323695e
IAmKou <luongcu111@gmail.com>
2024-11-10 17:16:29 +0700

    Update ClassServices.java

--- src/main/java/com/example/jip/services/ClassServices.java
+++ src/main/java/com/example/jip/services/ClassServices.java
@@ -1,4 +1,17 @@
 package com.example.jip.services;
 
+import com.example.jip.repository.ClassRepository;
+import com.example.jip.repository.ListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClassServices {
+    @Autowired
+    private ClassRepository classRepository;
+
+    @Autowired
+    private ListRepository listRepository;
+
+
 }
