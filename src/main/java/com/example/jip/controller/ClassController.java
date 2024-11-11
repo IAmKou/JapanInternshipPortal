177772ce55fbcefe54eeab9f47c3b77e3a7c9fce
IAmKou <luongcu111@gmail.com>
2024-11-12 00:04:39 +0700

    Update ClassController.java

--- src/main/java/com/example/jip/controller/ClassController.java
+++ src/main/java/com/example/jip/controller/ClassController.java
@@ -1,13 +1,11 @@
 package com.example.jip.controller;
 
 import com.example.jip.dto.ClassDTO;
-import com.example.jip.dto.TeacherDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.jip.services.ClassServices;
 import com.example.jip.entity.Class;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/class")
@@ -16,20 +14,16 @@ public class ClassController {
     private ClassServices classServices;
 
     @PostMapping("/create")
-    public String createClass(@RequestParam String className, @RequestParam int teacherId,  @RequestBody List<Integer> studentIds) {
-        ClassDTO classDTO = new ClassDTO();
-        classDTO.setName(className);
-        classDTO.setNumberOfStudents(studentIds.size());
-
-        TeacherDTO teacherDTO = new TeacherDTO();
-        teacherDTO.setId(teacherId);
-        classDTO.setTeacher(teacherDTO);
-
-        Class savedClass = classServices.saveClassWithStudents(classDTO, studentIds);
+    public String createClass(@RequestBody ClassDTO classDTO) {
+        if (classDTO.getName() == null || classDTO.getName().isEmpty()) {
+            throw new IllegalArgumentException("Class name is required");
+        }
+        if (classDTO.getTeacher() == null || classDTO.getTeacher().getId() == 0) {
+            throw new IllegalArgumentException("Teacher ID is required");
+        }
+        Class savedClass = classServices.saveClassWithStudents(classDTO, classDTO.getStudentIds());
         return "Class " + savedClass.getName() + " created with ID: " + savedClass.getId();
-
-
-
     }
+
 
 }
