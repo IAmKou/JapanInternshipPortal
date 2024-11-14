b941c592b27ac5a43d4315148b559a05b7ce7c54
toobi-svg <richchoi6@gmail.com>
2024-11-14 12:31:35 +0700

    Update MaterialDetails

--- src/main/java/com/example/jip/dto/MaterialDTO.java
+++ src/main/java/com/example/jip/dto/MaterialDTO.java
@@ -1,71 +1,71 @@
-package com.example.jip.dto;
+    package com.example.jip.dto;
 
-import java.util.Date;
+    import java.util.Date;
 
-public class MaterialDTO {
-    private int id;
-    private String title;
-    private String content;
-    private String img;
-    private Date created_date;
-    private TeacherDTO teacher;
+    public class MaterialDTO {
+        private int id;
+        private String title;
+        private String content;
+        private String img;
+        private Date created_date;
+        private TeacherDTO teacher;
 
-    public MaterialDTO() {}
+        public MaterialDTO() {}
 
-    public MaterialDTO(int id, String title, String content, String img, Date created_date, TeacherDTO teacher) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.img = img;
-        this.created_date = created_date;
-        this.teacher = teacher;
+        public MaterialDTO(int id, String title, String content, String img, Date created_date, TeacherDTO teacher) {
+            this.id = id;
+            this.title = title;
+            this.content = content;
+            this.img = img;
+            this.created_date = created_date;
+            this.teacher = teacher;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getImg() {
+            return img;
+        }
+
+        public void setImg(String img) {
+            this.img = img;
+        }
+
+        public Date getCreated_date() {
+            return created_date;
+        }
+
+        public void setCreated_date(Date created_date) {
+            this.created_date = created_date;
+        }
+
+        public TeacherDTO getTeacher() {
+            return teacher;
+        }
+
+        public void setTeacher(TeacherDTO teacher) {
+            this.teacher = teacher;
+        }
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public Date getCreated_date() {
-        return created_date;
-    }
-
-    public void setCreated_date(Date created_date) {
-        this.created_date = created_date;
-    }
-
-    public TeacherDTO getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(TeacherDTO teacher) {
-        this.teacher = teacher;
-    }
-}
