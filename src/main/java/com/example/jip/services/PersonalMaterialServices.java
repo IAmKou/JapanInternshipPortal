c81b8f2b39508a2515905deb5854b0e043f0a78e
toobi-svg <richchoi6@gmail.com>
2024-11-17 21:25:15 +0700

    update Material for Student

--- src/main/java/com/example/jip/services/PersonalMaterialServices.java
+++ src/main/java/com/example/jip/services/PersonalMaterialServices.java
@@ -1,20 +1,12 @@
 package com.example.jip.services;
 
-import com.example.jip.dto.MaterialDTO;
-import com.example.jip.dto.response.PersonalMaterialDTO;
-import com.example.jip.entity.Material;
+import com.example.jip.dto.PersonalMaterialDTO;
 import com.example.jip.entity.PersonalMaterial;
 import com.example.jip.entity.Student;
-import com.example.jip.entity.Teacher;
-import com.example.jip.repository.MaterialRepository;
 import com.example.jip.repository.PersonalMaterialRepository;
 import com.example.jip.repository.StudentRepository;
-import com.example.jip.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.List;
 
 @Service
 public class PersonalMaterialServices {
@@ -36,5 +28,6 @@ public class PersonalMaterialServices {
 
         return personalMaterialRepository.save(personalMaterial); // Sửa lỗi ở đây
     }
+
 
 }
