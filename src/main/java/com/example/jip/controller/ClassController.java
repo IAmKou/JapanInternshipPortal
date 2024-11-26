3c8f669ea03a988c2e514bf9b282cfea6ec724b5
IAmKou <luongcu111@gmail.com>
2024-11-26 11:53:25 +0700

    update view class for teacher

--- src/main/java/com/example/jip/controller/ClassController.java
+++ src/main/java/com/example/jip/controller/ClassController.java
@@ -66,8 +66,8 @@ public class ClassController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/getByTid")
-    public List<ClassDTO> getClassByTid(@RequestParam int teacherId) {
+    @GetMapping("/getByTid/{teacherId}")
+    public List<ClassDTO> getClassByTid(@PathVariable int teacherId) {
         return classRepository.findByTeacherId(teacherId).stream()
                 .map(ClassDTO::new)
                 .collect(Collectors.toList());
--- src/main/java/com/example/jip/entity/Attendant.java
+++ src/main/java/com/example/jip/entity/Attendant.java
@@ -1,18 +1,30 @@
 package com.example.jip.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
-
+@Entity
+@Table(name = "Attendant")
 @Setter
 @Getter
 public class Attendant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+
     private int student_id;
     private int schedule_id;
+
+
     private String status;
+
+    @Column(name="Date")
     private Date date;
+
+    @Column(name = "note")
     private String note;
 
     public Attendant(){}
