3c6d7c8f0e492b64942880ecda6707a989da6137
IAmKou <luongcu111@gmail.com>
2024-11-26 18:21:18 +0700

    Update ScheduleRepository.java

--- src/main/java/com/example/jip/repository/ScheduleRepository.java
+++ src/main/java/com/example/jip/repository/ScheduleRepository.java
@@ -3,6 +3,12 @@ package com.example.jip.repository;
 import com.example.jip.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
+import java.util.Optional;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
+
+    Optional<Schedule> findByClassIdAndDate(int classId, Date date);
+
 
 }
