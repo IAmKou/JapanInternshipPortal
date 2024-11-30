68769eb8de7115a0c362baabdd47b634c105efef
IAmKou <luongcu111@gmail.com>
2024-11-30 10:36:39 +0700

    update securityConfig

--- src/main/java/com/example/jip/repository/AccountRepository.java
+++ src/main/java/com/example/jip/repository/AccountRepository.java
@@ -10,7 +10,5 @@ import java.util.Optional;
 public interface AccountRepository extends CrudRepository<Account, Integer> {
 
     Optional<Account> findByUsername(String username);
-
-
-
+    boolean existsByUsername(String username);
 }
