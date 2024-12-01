ecf8b1b90765089e66873865d2f5c5a45f8e7b0c
Anh Long Cam Kiem <nguyenxuanphilong@gmail.com>
2024-12-02 01:42:11 +0700

    forum phan quyen 90%

--- src/main/java/com/example/jip/repository/AccountRepository.java
+++ src/main/java/com/example/jip/repository/AccountRepository.java
@@ -1,10 +1,7 @@
 package com.example.jip.repository;
+
 import com.example.jip.entity.Account;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends CrudRepository<Account, Integer> {
--- src/main/java/com/example/jip/repository/ThreadRepository.java
+++ src/main/java/com/example/jip/repository/ThreadRepository.java
@@ -1,8 +1,10 @@
 package com.example.jip.repository;
 
 import com.example.jip.entity.Thread;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +12,8 @@ import java.util.Optional;
 
 @Repository
 public interface ThreadRepository extends JpaRepository<Thread, Integer> {
-    Optional<Thread> findById(Integer id);
+    // Custom query to fetch threads with pagination
+    @Query("SELECT t FROM Thread t ORDER BY t.dateCreated DESC") // Example custom query
+    Page<Thread> getThreads(Pageable pageable);
 }
 
