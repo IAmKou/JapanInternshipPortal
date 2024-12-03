41f5b1ee518926f90057df39f8aec83cabd18514
IAmKou <luongcu111@gmail.com>
2024-12-03 20:36:24 +0700

    create edit-attendant

--- src/main/java/com/example/jip/repository/ScheduleRepository.java
+++ src/main/java/com/example/jip/repository/ScheduleRepository.java
@@ -1,15 +1,14 @@
 package com.example.jip.repository;
 
 import com.example.jip.dto.ScheduleDTO;
-import com.example.jip.dto.StudentScheduleDTO;
 import com.example.jip.entity.Schedule;
-import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
