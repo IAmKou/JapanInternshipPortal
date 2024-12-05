4c5f503930ab6d5c4be23cc1786d1fabb150f003
IAmKou <luongcu111@gmail.com>
2024-12-05 17:08:25 +0700

    Update StudentServices.java

--- src/main/java/com/example/jip/services/StudentServices.java
+++ src/main/java/com/example/jip/services/StudentServices.java
@@ -1,11 +1,14 @@
 package com.example.jip.services;
 
+import com.example.jip.dto.response.CloudinaryResponse;
 import com.example.jip.entity.Account;
 import com.example.jip.entity.Student;
 import com.example.jip.repository.AccountRepository;
 import com.example.jip.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import java.sql.Date;
 import java.util.Optional;
@@ -18,12 +21,20 @@ import java.util.Optional;
         @Autowired
         private AccountRepository accountRepository;
 
-        public Student createStudent(String fullname, String japanname, Date dob, String gender, String phoneNumber, String email, String img, String passport, int accountId) {
+        @Autowired
+        private CloudinaryService cloudinaryService;
+
+        public Student createStudent(String fullname, String japanname, Date dob, String gender, String phoneNumber, String email, MultipartFile img, MultipartFile passport, int accountId) {
             // Check if the account exists
             Optional<Account> accountOpt = accountRepository.findById(accountId);
             if (!accountOpt.isPresent()) {
                 throw new IllegalArgumentException("No account found with id: " + accountId);
             }
+
+            CloudinaryResponse response = cloudinaryService.uploadFileToFolder(img, "Account/" );
+            String imgUrl =  response.getUrl();
+            CloudinaryResponse responses = cloudinaryService.uploadFileToFolder(passport, "Account/" );
+            String passUrl =  response.getUrl();
 
             // Create a new Student object and set its properties
             Student student = new Student();
@@ -33,7 +44,7 @@ import java.util.Optional;
             student.setGender(Student.Gender.valueOf(gender));
             student.setPhoneNumber(phoneNumber);
             student.setEmail(email);
-            student.setImg(img);
+            student.setImg(imgUrl);
             student.setAccount(accountOpt.get());
             student.setMark(false);
 
