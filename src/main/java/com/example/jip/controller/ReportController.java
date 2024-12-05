6f0b1a2ef9f833ba0946eb72c7c22aecb2064189
IAmKou <luongcu111@gmail.com>
2024-12-05 12:33:42 +0700

    update request update profile

--- src/main/java/com/example/jip/controller/ReportController.java
+++ src/main/java/com/example/jip/controller/ReportController.java
@@ -1,10 +1,11 @@
 package com.example.jip.controller;
 
 import com.example.jip.dto.ReportDTO;
-import com.example.jip.entity.Report;
 import com.example.jip.repository.ReportRepository;
 import com.example.jip.services.ReportServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,10 +22,15 @@ public class ReportController {
 
 
     @PostMapping("/create")
-    public String createReport(@RequestParam String title, @RequestParam String content, @RequestParam int reporter_id) {
-        reportServices.createReport(title, content, reporter_id);
-        return "success";
+    public ResponseEntity<String> createReport(@RequestParam String title, @RequestParam String content, @RequestParam int uid) {
+        try {
+            reportServices.createReport(title, content, uid);
+            return ResponseEntity.ok("Report submitted successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create report: " + e.getMessage());
+        }
     }
+
 
     @GetMapping("/get")
     public List<ReportDTO> reportList(){
