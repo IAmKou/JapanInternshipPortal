cd678fa512f3451d0414315aeb2aabd85d1d16d7
toobi-svg <richchoi6@gmail.com>
2024-12-21 15:17:49 +0700

    update launguae

--- src/main/java/com/example/jip/controller/ImportAccountController.java
+++ src/main/java/com/example/jip/controller/ImportAccountController.java
@@ -1,14 +1,15 @@
 package com.example.jip.controller;
 
-import com.example.jip.repository.AccountRepository;
 import com.example.jip.services.AccountImportServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("account/import")
@@ -17,20 +18,31 @@ public class ImportAccountController {
     @Autowired
     AccountImportServices accountImportServices;
 
+    @Autowired
+    MessageSource messageSource; // Sử dụng MessageSource
+
     @PostMapping("/excel")
-    public ResponseEntity<?> importFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> importFile(@RequestParam("file") MultipartFile file, @RequestHeader(value = "Accept-Language", defaultValue = "en") String lang) {
+        Locale locale = new Locale(lang);  // Lấy Locale từ header Accept-Language
+
         if (!file.getOriginalFilename().endsWith(".xlsx")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(List.of("Invalid file format. Please upload an Excel file."));
+            String errorMessage = messageSource.getMessage("file.invalidFormat", null, locale);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(List.of(errorMessage));
         }
+
         try {
             List<String> errors = accountImportServices.importAccounts(file);
 
             if (!errors.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
             }
-            return ResponseEntity.ok(List.of("Data imported successfully"));
+
+            // Trả về thông điệp quốc tế hóa
+            String successMessage = messageSource.getMessage("file.importSuccess", null, locale);
+            return ResponseEntity.ok(List.of(successMessage));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of("Error occurred: " + e.getMessage()));
+            String errorMessage = messageSource.getMessage("error.general", new Object[] {e.getMessage()}, locale);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of(errorMessage));
         }
     }
 }
