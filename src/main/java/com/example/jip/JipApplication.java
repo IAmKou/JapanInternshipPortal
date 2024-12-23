4dce28d5d39124b4fd3f10762b88132c0e6bc21d
Anh Long Cam Kiem <nguyenxuanphilong@gmail.com>
2024-12-24 01:44:19 +0700

    export info be unchecked

--- src/main/java/com/example/jip/JipApplication.java
+++ src/main/java/com/example/jip/JipApplication.java
@@ -1,14 +1,18 @@
 package com.example.jip;
 
+import com.example.jip.configuration.ConfigProperties;
 import com.example.jip.services.AccountServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.Elements;
+
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ConfigProperties.class)
 public class JipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JipApplication.class, args);
