60a09ff5122cc856ea96123d87ea8c0573dea2e3
vinhji <84194738+vinhji@users.noreply.github.com>
2024-12-27 04:22:08 +0700

    shit

--- src/main/java/com/example/jip/entity/MarkReport.java
+++ src/main/java/com/example/jip/entity/MarkReport.java
@@ -20,7 +20,6 @@ public class MarkReport {
     @OneToOne
     @JoinColumn(name = "student_id", referencedColumnName = "id", nullable = false)
     Student student;
-    private String comment;
     @Column(name = "softskill")
     BigDecimal softskill;
     @Column(name = "avg_exam_mark")
