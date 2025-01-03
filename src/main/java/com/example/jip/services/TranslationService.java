f5a95125f1c4d1eb5e3003b8ce1e3b7678f53f0a
toobi-svg <richchoi6@gmail.com>
2025-01-03 13:46:51 +0700

    fic

--- src/main/java/com/example/jip/services/TranslationService.java
+++ src/main/java/com/example/jip/services/TranslationService.java
@@ -1,4 +1,31 @@
 package com.example.jip.services;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TranslationService {
+
+    private final Translate translate;
+
+    // Nhận API Key từ application.properties
+    public TranslationService(@Value("${google.cloud.api.key}") String apiKey) {
+        this.translate = TranslateOptions.newBuilder()
+                .setApiKey(apiKey) // Sử dụng API Key
+                .build()
+                .getService();
+    }
+
+    // Hàm dịch văn bản
+    public String translateText(String text, String targetLanguage) {
+        Translation translation = translate.translate(
+                text,
+                Translate.TranslateOption.targetLanguage(targetLanguage),
+                Translate.TranslateOption.format("text")
+        );
+        return translation.getTranslatedText();
+    }
 }
