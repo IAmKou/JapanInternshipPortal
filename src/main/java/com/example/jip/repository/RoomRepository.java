b8f0382dd545f06f3818345218b4cdba7dfdb151
IAmKou <luongcu111@gmail.com>
2025-01-03 12:53:04 +0700

    update

--- src/main/java/com/example/jip/repository/RoomRepository.java
+++ src/main/java/com/example/jip/repository/RoomRepository.java
@@ -2,12 +2,20 @@ package com.example.jip.repository;
 
 
 import com.example.jip.entity.Room;
+import com.example.jip.entity.RoomAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.sql.Date;
 import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
 Optional<Room> findByName(String name);
     boolean existsByName(String name);
-
+    @Query(value = "SELECT r.* FROM Room r " +
+            "JOIN room_availability ra ON r.id = ra.room_id " +
+            "WHERE ra.status = :status AND ra.date = :date LIMIT 1",
+            nativeQuery = true)
+    Optional<Room> findFirstAvailableRoomByStatusAndDate(@Param("status") String status, @Param("date") Date date);
 }
