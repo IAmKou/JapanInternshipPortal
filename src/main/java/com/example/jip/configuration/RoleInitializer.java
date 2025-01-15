febe2e3b432c683e15ecb00e6ef859a409d88e53
IAmKou <luongcu111@gmail.com>
2025-01-15 17:10:30 +0700

    Create RoleInitializer.java

---
+++ src/main/java/com/example/jip/configuration/RoleInitializer.java
@@ -0,0 +1,29 @@
+package com.example.jip.configuration;
+
+import com.example.jip.entity.Role;
+import com.example.jip.repository.RoleRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleInitializer {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @PostConstruct
+    public void initRoles() {
+        createRoleIfNotFound("MANAGER");
+        createRoleIfNotFound("STUDENT");
+        createRoleIfNotFound("TEACHER");
+    }
+
+    private void createRoleIfNotFound(String roleName) {
+        if (roleRepository.findByName(roleName).isEmpty()) {
+            Role role = new Role();
+            role.setName(roleName);
+            roleRepository.save(role);
+        }
+    }
+}
