bccae01c623309ccc4ce704ecc324e4874ab1e16
IAmKou <luongcu111@gmail.com>
2025-01-18 16:26:50 +0700

    Update RoomAvailability.java

--- src/main/java/com/example/jip/entity/RoomAvailability.java
+++ src/main/java/com/example/jip/entity/RoomAvailability.java
@@ -34,6 +34,7 @@ public class RoomAvailability {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "schedule_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Schedule schedule;
 
     @ManyToOne(fetch = FetchType.LAZY)
